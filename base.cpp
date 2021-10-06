ff5ca9d7fd9c3bb7a0e1b82be3f2c42e2996bac9
brotherick <erickjeronimo1@gmail.com>
2021-10-06 00:30:29 -0600

    save book in file

--- base.cpp
+++ base.cpp
@@ -1,11 +1,43 @@
 #include <stdio.h>
 #include <iostream>
 
+/*
+author: Erick Jeronimo
+*/
+
 using namespace std;
 
-int main(void){
+void saveBook()
+{
+	//declare variables
+	int id;
+	char bookName[90];
+	char author[90];
+	char publishYear[90];
+	FILE *books;
 
-	cout << "Hola mundo!";
+	//ask for data
+	cout << "Registrar libro";
+	cout << "Escribe el nombre del libro \n";
+	cin.getline(bookName, 90);
+	cout << "Escribe el autor del libro \n";
+	cin.getline(author, 90);
+	cout << "Escribe el anio de publicacion del libro \n";
+	cin >> publishYear;
 
-	//registro de libros
+	//open book file
+	books = fopen("./libros.txt", "a");	
+
+	//validate books exist
+	if (books != NULL) {
+	 	fprintf(books, "%s|%s|%s\n",bookName,author,publishYear);
+		fclose(books);
+	}else{
+		cout << " Error al Abrir el archivo libros.txt ";
+	}
 }
+
+int main(void){
+	saveBook();
+}
+
