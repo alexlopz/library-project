77845b3dbbda27e82d801ce7f05aa98e9f01f36f
Alex Lopez <alexanderlopez@outlook.com>
2021-10-10 21:46:02 -0600

    registerBookLoan method

--- base.cpp
+++ base.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 /*
-author: Erick Jeronimo
+authors: 
+	Erick Jeronimo
+	Francisco Alexander Lopez, Carné: 5190-135530
 */
 void saveBook()
 {
@@ -18,6 +20,7 @@ void saveBook()
 	//ask for data
 	cout << "Registrar libro";
 	cout << "Escribe el nombre del libro \n";
+	cin.ignore();
 	cin.getline(bookName, 90);
 	cout << "Escribe el autor del libro \n";
 	cin.getline(author, 90);
@@ -34,9 +37,104 @@ void saveBook()
 	}else{
 		cout << " Error al Abrir el archivo libros.txt ";
 	}
+
 }
 
+void menuBookLoans();
+void registerBookLoan();
+
 int main(void){
-	saveBook();
+
+	int option;
+	
+	cout << "INGRESA UNA OPCION POR FAVOR \n\n";
+	cout << "1) Registrar libros. \n";
+	cout << "2) Prestamos de libros. \n";
+	cout << "0) Salir \n";
+	cin >> option;
+	
+	while(option != 0) {
+		switch(option){
+			case 1:
+				saveBook();
+				break;
+			case 2:
+				menuBookLoans();
+				break;
+			default:
+				cout << "Opcion no registrada";
+				break;
+		}	
+	}	
 }
+
+void menuBookLoans(){
+	int option;
+	cout << "1) Registar prestamo \n";
+	cout << "0) Regresar al menu principal \n";
+	cin >> option; 
+	
+	switch(option){
+		case 1:
+			registerBookLoan();
+			break;
+		case 0:
+			main();
+			break;
+		default:
+			cout << "Error";
+			break;		
+	}
+	
+}
+
+void registerBookLoan() {
+	
+	//declare variables
+	FILE *loans;
+ 	char bookCode[8];
+ 	char loanDate[16];
+ 	char returDate[16];
+ 	char userName[128];
+ 	char userDocument[128];
+ 	
+ 	//initialize the file variable and pass the file path
+ 	loans = fopen("./prestamos.txt", "a");
+ 	
+ 	//validate that the prestamos.txt file exists and is not corrupted to start the flow
+ 	if(loans != NULL) {
+ 		
+ 		//capture data
+		cout << "**Registrar prestamos de libros** \n";
+		
+		cout << "Escribe el codigo del libro por favor:\n";
+		cin.ignore();
+		cin.getline(bookCode, 8);
+		
+		cout << "Escribe la fecha del prestamo ejemplo: 01/01/2021 \n";
+		cin.getline(loanDate, 16);
+		
+		cout << "Escribe la fecha de devolucion del libro ejemplo: 01/01/2021 \n";
+		cin.getline(returDate, 16);
+		
+		cout << "Nombre de quien presta el libro \n";
+		cin.getline(userName, 128);
+		
+		cout << "Numero de DPI de quien presta el libro \n";
+		cin.getline(userDocument, 128);
+		
+		//Write and format the captured data to the file.
+		fprintf(loans, "%s|%s|%s|%s|%s\n", bookCode, loanDate, returDate, userName, userDocument);
+		fclose(loans);
+		
+		cout << "Registro guardado con exito!!!\n\n";
+		
+	} else {//Error message if the file does not exist or is corrupted
+		cout << " Error al abrir el archivo prestamos.txt ";
+	}
+	 		
+	return;	
+ 	
+}
+
 
